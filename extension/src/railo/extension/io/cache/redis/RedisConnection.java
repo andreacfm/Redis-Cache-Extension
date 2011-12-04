c2be29c5bbe5cf8a45c7426f42bc274acbc52b5e
Andrea Campolonghi <acampolonghi@gmail.com>
2011-12-04 21:40:11 +0100

    implementign RedisConn

--- extension/src/railo/extension/io/cache/redis/RedisConnection.java
+++ extension/src/railo/extension/io/cache/redis/RedisConnection.java
@@ -1,8 +1,27 @@
 package railo.extension.io.cache.redis;
 
+import redis.clients.jedis.Jedis;
+
 public class RedisConnection {
 
-    public RedisConnection() {
+    private Jedis instance;
 
+    private RedisConnection() {}
+
+    public Jedis init(){
+
+        if(instance != null){
+            return instance;
+        }
+
+        instance = new Jedis("host", 1111);
+
+        return instance;
     }
+
+    public Jedis getInstance(){
+        return instance;
+    }
+
+
 }
